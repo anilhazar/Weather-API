821edd96f160d5e213a8513cd39c9e215e726575
Ali Anıl Hazar <112640395+anilhazar@users.noreply.github.com>
2024-03-16 12:34:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,18 +13,14 @@ app.get("/", function(req,res){
 })
 
 app.post("/", function(req,res){
-   let query = req.body.cityName; // html de forma atadığımız değere ulaşıyoruz.
+   let query = req.body.cityName;
    let url = "https://api.openweathermap.org/data/2.5/weather?q="+ query+ "&appid=bb3e52412a78b7315d5341d09133e1fe&units=metric";
-    https.get(url, function(response){ // bir api nesnesi oluşturup, eğer bu apiye veri gelirse nesneyi veri çekmek için kullanacağız.
-       // console.log(response);
+    https.get(url, function(response){ 
         
-     response.on("data", function(data){  // response nesnesine gelen datayı event listener ekleyerek function olasılığı yaratıyor. 
+     response.on("data", function(data){   
         
-       //console.log(data); // data is JSON FILE hexadecimal 
-        let weatherJSON = JSON.parse(data) // bu datayı JSON File şekline dönüştürüyoruz ve bir node objesine atıyoruz. 
-        //console.log(weatherJSON);
-        let weatherTemp = weatherJSON.main.temp // burada JSON ağacını kontrol ederek kendine path yapabilirsin veya keyin pathini kopyalayabilirsin. 
-        //console.log(weatherTemp); // burada temp değerine ulaşıp onu bir variable atıyoruz. sonuçta weatherJSON diye bir nesneye atadık tüm datayı 
+        let weatherJSON = JSON.parse(data) 
+        let weatherTemp = weatherJSON.main.temp 
         let weatherState = weatherJSON.weather[0].description
         let weatherIcon = weatherJSON.weather[0].icon
         let imgUrl = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
